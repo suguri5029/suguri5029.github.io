d5ee962ca52e59e7f60df02165cf2028fcb47d46
suguri5029 <suguri5029@outlook.com>
2024-01-13 00:12:22 +0800

    Site updated: 2024-01-13 00:12:21

--- js/search.js
+++ js/search.js
@@ -1,120 +1,78 @@
-// A local search script with the help of [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
-// Copyright (C) 2015 
-// Joseph Pan <http://github.com/wzpan>
-// Shuhao Mao <http://github.com/maoshuhao>
-// This library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2.1 of the
-// License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-// 02110-1301 USA
-// 
+// Global searchConfig
 
-var searchFunc = function(path, search_id, content_id) {
-    'use strict';
-    $.ajax({
-        url: path,
-        dataType: "xml",
-        success: function( xmlResponse ) {
-            // get the contents from search data
-            var datas = $( "entry", xmlResponse ).map(function() {
-                return {
-                    title: $( "title", this ).text(),
-                    content: $("content",this).text(),
-                    url: $( "url" , this).text()
-                };
-            }).get();
+document.addEventListener('DOMContentLoaded', () => {
 
-            var $input = document.getElementById(search_id);
-      if (!$input) return;
-            var $resultContent = document.getElementById(content_id);
+  const input = document.querySelector('.search-input');
+  const container = document.querySelector('.search-result-container');
 
-            $input.addEventListener('input', function(){
-                var str='<ul class=\"search-result-list\">';
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
-                $resultContent.innerHTML = "";
-                if (this.value.trim().length <= 0) {
-                    return;
-                }
-                // perform local searching
-                datas.forEach(function(data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    if (!data.title || data.title.trim() === '') {
-                        data.title = "Untitled";
-                    }
-                    var data_title = data.title.trim().toLowerCase();     
-                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
-                    // only match artiles with not empty contents
-                    if (data_content !== '') {
-                        keywords.forEach(function(keyword, i) {
-                            index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
+  const localSearch = new LocalSearch({
+    path             : searchConfig.path,
+    top_n_per_article: searchConfig.top_n_per_article,
+    unescape         : searchConfig.unescape
+  });
 
-                            if( index_title < 0 && index_content < 0 ){
-                                isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
-                                // content_index.push({index_content:index_content, keyword_len:keyword_len});
-                            }
-                        });
-                    } else {
-                        isMatch = false;
-                    }
-                    // show search results
-                    if (isMatch) {
-                        str += "<li><a href='"+ data_url +"' class='search-result-title'>"+ data_title +"</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g,"");
-                        if (first_occur >= 0) {
-                            // cut out 100 characters
-                            var start = first_occur - 20;
-                            var end = first_occur + 80;
+  if (searchConfig.preload) {
+    // preload the search data when the page loads
+    console.log("loading page");
+    localSearch.fetchData();
+  }
 
-                            if(start < 0){
-                                start = 0;
-                            }
+  function openSearchPopup() {
+    document.querySelector('.search-popup').classList.add('search-activate');
+    if (!localSearch.isfetched) {
+      localSearch.fetchData();
+    } 
+  }
 
-                            if(start == 0){
-                                end = 100;
-                            }
+  function closeSearchPopup() {
+    document.querySelector('.search-popup').classList.remove('search-activate');
+    // refresh search box
+    input.value = '';
+    container.innerHTML = `<div class="search-result-message" ></div>`;
+  }
 
-                            if(end > content.length){
-                                end = content.length;
-                            }
+  // open search box
+  document.querySelector('.search-btn').addEventListener('click', openSearchPopup);
 
-                            var match_content = content.substr(start, end); 
+  // close search box
+  document.querySelector('.search-popup-overlay').addEventListener('click', closeSearchPopup);
+  document.querySelector('.search-close-btn').addEventListener('click', closeSearchPopup);
 
-                            // highlight all keywords
-                            keywords.forEach(function(keyword){
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em class=\"search-keyword\">"+keyword+"</em>");
-                            });
+  function displaySearchResult() {
+    if (!localSearch.isfetched) return;
+    const searchText = input.value.trim().toLowerCase();
+    const keywords = searchText.split(/[-\s]+/);
+    if (searchText.length > 0) {
+      resultItems = localSearch.getResultItems(keywords);
+    }
 
-                            str += "<p class=\"search-result\">" + match_content +"...</p>"
-                        }
-                        str += "</li>";
-                    }
-                });
-                str += "</ul>";
-                $resultContent.innerHTML = str;
-            });
-        }
-    });
-}
+    if (keywords.length === 1 && keywords[0] === '') {
+      // no input
+      container.innerHTML = `<div class="search-result-message" ></div>`
+    } else if (resultItems.length === 0) {
+      // no result
+      container.innerHTML = `<div class="search-result-message" >No result found</div>`;
+    } else {
+      // display result(s)
+      container.innerHTML = `
+      <div class="search-result-message">${resultItems.length} result(s) found</div>
+      <ul class="search-result-list">${resultItems.map(result => result.item).join('<div class="h-line-secondary"></div>')}
+      </ul>`;
+    }
+
+  };
+
+  if (searchConfig.trigger == 'auto') {
+    // whenever there is input, update search result
+    input.addEventListener('input', displaySearchResult);
+  } else {
+    // update search result when press "enter"
+    input.addEventListener('keypress', event => {
+      if (event.key === 'Enter') {
+        displaySearchResult();
+      }
+    })
+  }
+  window.addEventListener('search:loaded', displaySearchResult);
+});
+  
